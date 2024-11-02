a3c7c68ee8da56fa8da8192ccd6aab72f4fe6bbb
Banyar Shin <banyar.minshin@gmail.com>
2024-11-01 23:01:45 -0700

    Backend setup 11/1/2024 (#4)
    * reinitialize backend with mongo

--- backend/app.py
+++ backend/app.py
@@ -1,41 +1,19 @@
-from flask import Flask, request, jsonify
-import pandas as pd
+from flask import Flask, render_template, request
 
-app = Flask(__name__)
+import subprocess as sp
+from pymongo import MongoClient
+from mongopass import mongopass
 
+app = Flask("CFTbackend")
 
-# @app.route('/')
-# def index():
-#     return render_template('index.html')
-
-
-@app.route("/upload", methods=["POST"])
-def upload_file():
-    if "file" not in request.files:
-        return jsonify({"error": "No file part"}), 400
-
-    file = request.files["file"]
-
-    if file.filename == "":
-        return jsonify({"error": "No selected file"}), 400
-
-    # Process CSV file
-
-    # Call method to process CSV using pandas
-
-    try:
-        df = pd.read_csv(file)
-        # Do some processing with the data, for example, sum a column
-        total_sum = df[
-            "column_name"
-        ].sum()  # Replace 'column_name' with your actual column
-
-        # Respond with some result from the processing
-        return jsonify({"total_sum": total_sum})
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
+client = MongoClient(mongopass)
+db = client.curd
+myCollection = db.myColl
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+@app.route("/")
+def home():
+    return "It works!"
 
+
+app.run(port=5000, debug=True)
