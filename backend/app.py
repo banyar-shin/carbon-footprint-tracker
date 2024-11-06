fe3f73a0b51cd6a90ed54d216938071315e5b2a8
Prabhnoor Khatkar <54859879+PrabhnoorKhatkar@users.noreply.github.com>
2024-11-06 01:18:27 -0800

    Add Upload Route & CSV Parser (#6)
    * add csv parser
    
    * add methods
    
    * Add Upload Route
    
    * Change file path to file obj

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 
 import subprocess as sp
 from pymongo import MongoClient
 from mongopass import mongopass
+import pandas as pd
+from main import *
 
 app = Flask("CFTbackend")
 
@@ -14,6 +16,37 @@ myCollection = db.myColl
 @app.route("/")
 def home():
     return "It works!"
+
+
+@app.route("/upload", methods=["POST"])
+def uploadCSV():
+      # Check if a file was uploaded
+    if 'file' not in request.files:
+        return jsonify({"error": "No file provided"}), 400
+    
+    file = request.files['file']
+    
+    # Ensure the file has content
+    if file.filename == '':
+        return jsonify({"error": "Empty filename"}), 400
+
+    # Read the CSV into a Pandas DataFrame to check its structure
+    try:
+        df = pd.read_csv(file, skiprows= 5, nrows=2) # Get first 2 rows
+        
+        # Check for specific columns to determine parsing method
+        if 'START DATE' in df.columns and 'END DATE' in df.columns:
+            return parseAnnualCSV(file)
+        elif 'START TIME' in df.columns and 'END TIME' in df.columns:
+           
+            return parseMonthCSV(file)
+        
+        else:
+            return jsonify({"error": "Unknown CSV format"}), 400
+    
+    except Exception as e:
+        return jsonify({"error": f"Failed to read CSV file: {str(e)}"}), 500
+    
 
 
 app.run(port=5000, debug=True)
--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,41 @@
 import pandas as pd
 
-dataFile = ""
+from datetime import datetime, timedelta
+
+def parseMonthCSV(file):
+    df = pd.read_csv(file,
+                                skiprows = 5 # skip the first 5 rows
+                                )
+            
+        # Drop the START TIME AND NOTES
+    df = df.drop(columns=['START TIME', 'NOTES'])
+
+        # Add 1 minute to each END TIME and rename it to TIME
+    df['TIME'] = df['END TIME'].apply(lambda x: (datetime.strptime(x, "%H:%M") + timedelta(minutes=1)).strftime("%H:%M"))
+    df = df.drop(columns=["END TIME"])
+    df['TYPE'] = df['TYPE'].replace("Electric usage", "Electric")
+
+    print(df.head(10))
+
+
+
+def parseAnnualCSV(file):
+    df = pd.read_csv(file,
+                                skiprows = 5 # skip the first 5 rows
+                                )
+        
+    df = df.drop(columns=['COST', 'START DATE', 'NOTES'])
+
+    df["END DATE"] = pd.to_datetime(df["END DATE"]).dt.to_period("M").astype(str)
+
+    df = df.rename(columns={'END DATE':'DATE'})
+    df['TYPE'] = df['TYPE'].replace("Electric billing", "Electric")
+
+    print(df.head(10))
+
+
+
+
+
+
+
