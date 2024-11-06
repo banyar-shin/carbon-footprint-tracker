9c89fcb5d2722b9695fde9df8cee7780e76edffb
Banyar Shin <banyar.minshin@gmail.com>
2024-11-06 04:29:41 -0800

    implement dashboard page and nested routes (#9)

--- frontend/src/routes/DashboardPage.jsx
+++ frontend/src/routes/DashboardPage.jsx
@@ -1,19 +1,11 @@
-import { Link } from 'react-router-dom'
+import { Outlet } from 'react-router-dom'
+import DashboardMenu from '../components/DashboardMenu'
 
 export default function DashboardPage() {
   return (
-    <>
-      <h1>Dashboard page</h1>
-      <p>This is a protected page.</p>
-
-      <ul>
-        <li>
-          <Link to="/dashboard/invoices">Invoices</Link>
-        </li>
-        <li>
-          <Link to="/">Return to index</Link>
-        </li>
-      </ul>
-    </>
+    <div className="max-h-[92vh] flex justify-start gap-5">
+      <DashboardMenu />
+      <Outlet />
+    </div>
   )
 }
--- frontend/src/routes/LandingPage.jsx
+++ frontend/src/routes/LandingPage.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 
 export default function LandingPage() {
   return (
-    <div className="flex flex-col h-screen w-full items-center justify-center text-center text-base-content bg-base-300 p-4">
+    <div className="flex flex-col h-screen w-full items-center justify-center text-center text-base-content bg-base-100 p-4">
       {/* Hero Section */}
       <h1 className="text-5xl font-bold text-base-content mb-4 animate-pulseSlow">Welcome to CarbonWise🍃</h1>
       <p className="text-lg text-base-content max-w-xl mb-8 ">
@@ -11,15 +11,15 @@ export default function LandingPage() {
 
       {/* Features Overview */}
       <div className="grid grid-cols-1 md:grid-cols-3 gap-6 w-full max-w-4xl ">
-        <Link to="/dashboard/transport" className="bg-base-100 shadow-md rounded-lg p-6 hover:bg-base-200 transition duration-300 ease-in-out hover:scale-105">
+        <Link to="/dashboard/transport" className="bg-base-100 shadow-lg rounded-lg p-6 hover:bg-base-200 transition duration-300 ease-in-out hover:scale-105">
           <h3 className="text-2xl font-semibold text-base-content mb-2 ">Transportation Tracker</h3>
           <p className="text-base-content">Track emissions from your daily travel and explore sustainable transport options.</p>
         </Link>
-        <Link to="/dashboard/energy" className="bg-base-100 shadow-md rounded-lg p-6 hover:bg-base-200 transition duration-300 ease-in-out hover:scale-105">
+        <Link to="/dashboard/energy" className="bg-base-100 shadow-lg rounded-lg p-6 hover:bg-base-200 transition duration-300 ease-in-out hover:scale-105">
           <h3 className="text-2xl font-semibold text-base-content mb-2">Energy Usage Monitoring</h3>
           <p className="text-base-content">Monitor your household energy usage and reduce idle energy consumption.</p>
         </Link>
-        <Link to="/dashboard/diet" className="bg-base-100 shadow-md rounded-lg p-6 hover:bg-base-200 transition duration-300 ease-in-out hover:scale-105">
+        <Link to="/dashboard/diet" className="bg-base-100 shadow-lg rounded-lg p-6 hover:bg-base-200 transition duration-300 ease-in-out hover:scale-105">
           <h3 className="text-2xl font-semibold text-base-content mb-2">Diet Analysis</h3>
           <p className="text-base-content">Analyze the carbon impact of your diet and get suggestions for eco-friendly choices.</p>
         </Link>
