63e6cc2ebdad7518cbf8134f30535acd562ba04a
Banyar Shin <banyar.minshin@gmail.com>
2024-12-11 06:09:11 -0800

    fix tutorial page (#37)

--- frontend/src/routes/Tutorial.jsx
+++ frontend/src/routes/Tutorial.jsx
@@ -28,45 +28,45 @@ export default function Tutorial() {
   };
 
   return (
-    <div className="p-8 bg-base-200 min-h-screen">
-      <h1 className="text-3xl font-bold mb-8 text-center">PG&E Download Tutorial</h1>
+    <div className="h-full overflow-y-auto p-4 items-center text-center text-base-content space-y-4">
+      <div className="w-full p-6 rounded-lg bg-base-200 space-y-6">
+        <h1 className="text-2xl font-bold mb-4">PG&E Download CSV Tutorial</h1>
+        <div className="flex-col space-y-4">
+          {/* Steps Section */}
+          <div className="bg-base-100 rounded-xl p-6">
+            <h2 className="text-xl font-semibold mb-4">Follow the steps below:</h2>
+            <ul className="space-y-4">
+              <li className={`p-3 rounded-lg transition-all duration-300 ${currentImageIndex === 0 ? 'bg-primary text-white shadow-md' : 'bg-base-200 text-base-content hover:bg-base-300'}`}>First head over to <a href="https://m.pge.com/" target="_blank" rel="noopener noreferrer">https://m.pge.com/</a> and Login in with account details.</li>
+              <li className={`p-3 rounded-lg transition-all duration-300 ${currentImageIndex === 1 ? 'bg-primary text-white shadow-md' : 'bg-base-200 text-base-content hover:bg-base-300'}`}>Secondly, click on energy usage details.</li>
+              <li className={`p-3 rounded-lg transition-all duration-300 ${currentImageIndex === 2 ? 'bg-primary text-white shadow-md' : 'bg-base-200 text-base-content hover:bg-base-300'}`}>Thirdly, click Green Download Data Button.</li>
+              <li className={`p-3 rounded-lg transition-all duration-300 ${currentImageIndex === 3 ? 'bg-primary text-white shadow-md' : 'bg-base-200 text-base-content hover:bg-base-300'}`}>Finally, You can download any timeframe. We recommend doing based on monthly billing cycle.</li>
+            </ul>
 
-      <div className="grid grid-cols-2 gap-8">
-        {/* Screenshot Section */}
-        <div className="bg-white rounded-xl shadow-lg overflow-hidden">
-          <img 
-            src={tutorialImages[currentImageIndex]} 
-            alt={`Tutorial Substep ${currentImageIndex + 1}`} 
-            className="w-full h-auto max-h-[70vh] object-contain mx-auto"
-          />
-        </div>
-
-        {/* Steps Section */}
-        <div className="bg-white rounded-xl p-6 shadow-lg">
-          <h2 className="text-xl font-semibold mb-4">Follow the steps below:</h2>
-          <ul className="space-y-4">
-            <li className={`p-3 rounded-lg transition-all duration-300 ${currentImageIndex === 0 ? 'bg-primary text-white shadow-md' : 'bg-base-200 text-base-content hover:bg-base-300'}`}>First head over to <a href="https://m.pge.com/" target="_blank" rel="noopener noreferrer">https://m.pge.com/</a> and Login in with account details.</li>
-            <li className={`p-3 rounded-lg transition-all duration-300 ${currentImageIndex === 1 ? 'bg-primary text-white shadow-md' : 'bg-base-200 text-base-content hover:bg-base-300'}`}>Secondly, click on energy usage details.</li>
-            <li className={`p-3 rounded-lg transition-all duration-300 ${currentImageIndex === 2 ? 'bg-primary text-white shadow-md' : 'bg-base-200 text-base-content hover:bg-base-300'}`}>Thirdly, click Green Download Data Button.</li>
-            <li className={`p-3 rounded-lg transition-all duration-300 ${currentImageIndex === 3 ? 'bg-primary text-white shadow-md' : 'bg-base-200 text-base-content hover:bg-base-300'}`}>Finally, You can download any timeframe. We recommend doing based on monthly billing cycle.</li>
-          </ul>
-
-          <div className="flex justify-between items-center mt-6">
-            <button 
-              onClick={handlePreviousImage} 
-              disabled={currentImageIndex === 0} 
-              className="btn btn-circle btn-outline"
-            >
-              <ChevronLeft />
-            </button>
-            <span className="text-lg">Step {currentImageIndex + 1} of {tutorialImages.length}</span>
-            <button 
-              onClick={handleNextImage} 
-              disabled={currentImageIndex === tutorialImages.length - 1} 
-              className="btn btn-circle btn-outline"
-            >
-              <ChevronRight />
-            </button>
+            <div className="flex justify-between items-center mt-6">
+              <button
+                onClick={handlePreviousImage}
+                disabled={currentImageIndex === 0}
+                className="btn btn-circle btn-outline"
+              >
+                <ChevronLeft />
+              </button>
+              <span className="text-lg">Step {currentImageIndex + 1} of {tutorialImages.length}</span>
+              <button
+                onClick={handleNextImage}
+                disabled={currentImageIndex === tutorialImages.length - 1}
+                className="btn btn-circle btn-outline"
+              >
+                <ChevronRight />
+              </button>
+            </div>
+          </div>
+          {/* Screenshot Section */}
+          <div className="bg-base-100 rounded-xl p-3">
+            <img
+              src={tutorialImages[currentImageIndex]}
+              alt={`Tutorial Substep ${currentImageIndex + 1}`}
+              className="w-full h-auto max-h-[70vh] rounded-lg object-contain mx-auto"
+            />
           </div>
         </div>
       </div>
